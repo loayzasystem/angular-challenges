faca1a996a5987a20d95a0aad8149692ac34861b
loayz <loayza.system@gmail.com>
2025-03-25 22:11:52 -0500

    feat: anchor navigation

--- apps/angular/21-anchor-navigation/src/app/app.routes.ts
+++ apps/angular/21-anchor-navigation/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Route } from '@angular/router';
-import { FooComponent } from './foo.component';
 import { HomeComponent } from './home.component';
 
 export const appRoutes: Route[] = [
   { path: 'home', component: HomeComponent },
-  { path: 'foo', component: FooComponent },
+  {
+    path: 'foo',
+    loadComponent: () => import('./foo.component').then((m) => m.FooComponent),
+  },
   { path: '**', redirectTo: 'home' },
 ];
